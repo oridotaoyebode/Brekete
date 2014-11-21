bd217960361275f0f42ae3c8bc93d0be080f12ea
Matt <matt@roguecode.co.za>
2014-11-21 17:14:25 +0200

    First

---
+++ Brekete/StreamPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using Microsoft.PlayerFramework.Adaptive;
+
+namespace Brekete
+{
+  public partial class StreamPage : PhoneApplicationPage
+  {
+    public StreamPage()
+    {
+      InitializeComponent();
+      player.Plugins.Add(new AdaptivePlugin());
+    }
+
+    protected override void OnNavigatedFrom(NavigationEventArgs e)
+    {
+      player.Dispose();
+      base.OnNavigatedFrom(e);
+    }
+  }
+}
